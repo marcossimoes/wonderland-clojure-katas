df8241d16b638129033433067f2e1ee22fd85d92
marcossimoes <marcos.simoes@nubank.com.br>
2020-03-13 16:09:51 -0300

    add first bruteforce solution for decipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,55 @@
 (ns alphabet-cipher.coder)
 
+(defn char->index
+  "receives a capital letter in char format and returns an index from 0 (A) to 25 (Z)"
+  [c]
+  (- (int c) 97))
+
+(defn index->letter
+  "receives a positive integer i and returns the corresponding letter where
+  0 = A, 1 = B ..., 25 = Z
+  26 = A, ..."
+  [non-neg-int]
+  (-> non-neg-int
+      (mod 26)
+      (+ 97)
+      (char)
+      (str)))
+
+(defn extended-kwd
+  "extended keyword equal or higher in size than the message"
+  [keyword message]
+  (nth (iterate (partial str keyword) "")
+       (inc (quot (count message)
+                  (count keyword)))))
+
+(defn encode-char
+  "given a keyword char and a message char, returns the encoded-char"
+  [k-char m-char]
+  (index->letter
+    (+ (char->index k-char)
+       (char->index m-char))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [extnd-kwd (extended-kwd keyword message)]
+    (apply str (map encode-char extnd-kwd message))))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn decode-char
+  [k-char m-char]
+  (index->letter
+    (+ 26
+       (- (char->index m-char)
+          (char->index k-char)))))
 
-(defn decipher [cipher message]
-  "decypherme")
+(defn decode
+  [keyword message]
+  (let [extnd-kwd (extended-kwd keyword message)]
+    (apply str (map decode-char extnd-kwd message))))
 
+(defn decipher
+  ([cipher message] (decipher cipher message 1))
+  ([cipher message num-parts]
+   (let [parts (partition num-parts (decode message cipher))]
+     (if #nu/tap (apply = parts)
+       #nu/tap (apply str (first parts))
+       (decipher cipher message (inc num-parts))))))
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest test-char->index
+  (testing "returns an index ranging from 0 (A) to 25 (Z)"
+    (is (= 0 (char->index \a)))
+    (is (= 9 (char->index \j)))
+    (is (= 10 (char->index \k)))
+    (is (= 22 (char->index \w)))
+    (is (= 24 (char->index \y)))
+    (is (= 25 (char->index \z)))))
+
+(deftest test-non-neg-int->letter
+  (testing "returns a letter ranging from a (0 or any multiple of 26)
+  to z (25 or any n that (n-25) is a multiple of 26"
+    (is (= "a" (index->letter 0)))
+    (is (= "b" (index->letter 1)))
+    (is (= "z" (index->letter 25)))
+    (is (= "a" (index->letter 26)))))
+
+(deftest test-extended-keyword
+  (testing "returns a a keyword that is equal or higher in size than the message"
+    (is (= "bolabolabolabolabolabolabolabola" (extended-kwd "bola" "anticonstitucionalissimamente")))
+    (is (= "bolabola" (extended-kwd "bola" "abobora")))
+    (is (= "bola" (extended-kwd "bola" "boi")))))
+
+(deftest test-encode-char
+  (testing
+    (is (= "e" (encode-char \m \s)))))
+
 (deftest test-encode
   (testing "can encode a message with a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
